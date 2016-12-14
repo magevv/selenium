edd190040d1a7bdaa140b344b29b0a4464b260b8
Yuriy Popsuenko <ypo@encode.dk>
2016-12-14 22:48:28 +0200

    test login scenario

---
+++ selenium-lessons/src/test/java/qa/lesson1/task3/Login.java
@@ -0,0 +1,35 @@
+package qa.lesson1.task3;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Login {
+
+    WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void login() {
+        driver.navigate().to("http:/localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+}
